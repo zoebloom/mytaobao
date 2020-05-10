5bd27d6cefbbbebcfeb2e4793e37ba88886cecc7
haozhe <582112159@qq.com>
2020-05-10 23:39:22 +0800

    这是第二次修改

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 let gulp = require("gulp");
 let connect = require("gulp-connect");
+let sass = require("gulp-sass");
+let htmlmin = require("gulp-htmlmin")
 
 gulp.task("watch-all",async ()=>{
-    gulp.watch("./src/**/*",async ()=>{
-        gulp.src("./src/**/*")
-        .pipe(gulp.dest("./dist"));
+    gulp.watch("./src/*.html",async ()=>{
+        gulp.src("./src/*.html")
+        .pipe(htmlmin({
+            collapseWhitespace:true
+        }))
+        .pipe(gulp.dest("./dist"))
     })
+
+    gulp.watch("./src/*.scss",async ()=>{
+        gulp.src("./src/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./dist/css"));
+     });
 
 
 })
